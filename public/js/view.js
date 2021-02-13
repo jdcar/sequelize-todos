9c6ce68bd91293c8dadf4594fcd985e812026e00
jdcar <jdcarlstone@gmail.com>
2021-02-13 12:00:02 -0600

    fetch

--- public/js/view.js
+++ public/js/view.js
@@ -1,5 +1,29 @@
 document.addEventListener('DOMContentLoaded', (e) => {
   console.log('DOM loaded!');
 
-  // your code here...
+  const form = document.getElementById('todo-form')
+  const newTodoInput = document.querySelector('input.new-item')
+
+  // select dom
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    const text = newTodoInput.value
+
+    fetch('/api/todos', {
+      method: 'POST',
+      body: JSON.stringify({text:text}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.error(err))
+
+  })
+
+  // input
+  // form
+  // add listeners
+  // ajax call
 });
